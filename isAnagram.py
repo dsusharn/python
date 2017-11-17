cde66d4a49968bc8af8e80a5bc91ba8cddaea798
Диана <diana@MacBook-Air-Diana.local>
2017-11-17 10:47:49 +0200

    returned to the previos version, fixed

--- isAnagram.py
+++ isAnagram.py
@@ -1,17 +1,18 @@
-def isAnagram (str):
-    i = 0
-    reverse = str[::-1]
-    while (i < len(str)):
-        if str[i] == reverse[i]:
-            i = i + 1
-        else: break
-    if i == len(str):
-        print "IS an anagram"
-        return 1   
-    else:
-        print "NOT an anagram"
-        return 0
- 
-a = input()
+def _is_anagram(str):
+    counter = 0
+    while (counter < len(str) / 2):
+        if str[counter] == str[len(str) - counter - 1]:
+            checker = True
+        else:
+            checker = False
+            break
+        counter += 1
+    if checker == True:
+        print('IS an anagram')
+    else: 
+        print('is NOT an anagram')
+    return checker
 
-isAnagram(a)
+input_string = input("Type a string in '': \n")
+
+_is_anagram(input_string)
